80278e6a98512788a5bff80ed1090fb4c34234d2
HyanCat <hyancat@live.cn>
2016-08-28 15:49:42 +0800

    initial.

---
+++ tests/AliyunDirectMailTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of LaravelPackageDemo.
+ *
+ * Created by HyanCat.
+ *
+ * Copyright (C) HyanCat. All rights reserved.
+ */
+class AliyunDirectMailTest extends TestCase
+{
+    public function testSingle()
+    {
+        
+    }
+
+    public function testBatch()
+    {
+
+    }
+}
